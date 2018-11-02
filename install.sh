0207ab8c4e41935a74cea5437f68b959605ae0ee
chendong12 <43974374+chendong12@users.noreply.github.com>
2018-11-02 16:53:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
+#复制下面的代码到你的服务器进行ssr服务器的安装
+#环境要求 CentOS 6，7 / Debian 6+ / Ubuntu 14.04 +
 wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssrmu.sh && chmod +x ssrmu.sh && bash ssrmu.sh
